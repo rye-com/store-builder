5369418ea4437316a61e281e00b9a5ccdf320ee0
Mikhail Shin <mikhail@rye.com>
2022-08-20 10:03:52 -0700

    Move solana stuff to server to reduce bundle size

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const { PORT } = require('./constants');
 const { getPageWithMetatags } = require('./getPageWithMetatags');
+const { getTokens, getNftMetadata } = require('./walletApi');
 
 const app = express();
 
@@ -12,6 +13,32 @@ app.use(
     index: false,
   })
 );
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/api/getTokens', (req, res, next) => {
+  const publicKeyStr = req.query['publickey'];
+  if (!publicKeyStr) {
+    res.status(400).end();
+  }
+  getTokens(publicKeyStr)
+    .then((tokens) => res.send(tokens))
+    .catch(() => res.status(500).end());
+});
+
+app.get('/api/getNftMetadata', (req, res, next) => {
+  const mintStr = req.query['mintstr'];
+  if (!mintStr) {
+    res.status(400).end();
+  }
+  getNftMetadata(mintStr)
+    .then((tokens) => res.send(tokens))
+    .catch(() => res.status(500).end());
+});
 
 // here we serve the index.html page
 app.get('/*', (req, res, next) => {
