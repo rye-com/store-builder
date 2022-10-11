e987abfeb4c26e85273e677bfce35a74e2d00743
Mikhail Shin <mikhail@rye.com>
2022-10-10 20:50:53 -0700

    Add API Key header

--- frontend/src/gql/client.js
+++ frontend/src/gql/client.js
@@ -3,6 +3,9 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL_URL,
   cache: new InMemoryCache(),
+  headers: {
+    Authorization: `Basic ${process.env.REACT_APP_GRAPHQL_KEY}`,
+  },
 });
 
 export function GraphQLProvider({ children }) {
